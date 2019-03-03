c773c4a7cb00027cf8084788767055574f69d213
Nicolas Martínez <remyblunt_@hotmail.com>
2019-03-03 19:06:12 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrincipalComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [PrincipalComponent]
+})
+export class AppModule { }
